5f730cfa52b7c537fc7f04dd370603f190e85c9f
Raam4 <ramiro_cardozo22@hotmail.com>
2022-03-19 12:50:10 -0300

    fix

--- src/main/java/com/raama/portfoliobackend/PortfolioBackendApplication.java
+++ src/main/java/com/raama/portfoliobackend/PortfolioBackendApplication.java
@@ -2,12 +2,25 @@ package com.raama.portfoliobackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class PortfolioBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PortfolioBackendApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("*").allowedOrigins("*");
+			}
+		};
 	}
 
 }
