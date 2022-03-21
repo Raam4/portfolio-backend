ee7af4e85281a41eef2a490800335f49c660ff92
Raam4 <ramiro_cardozo22@hotmail.com>
2022-03-21 15:25:01 -0300

    fix

--- src/main/java/com/raama/portfoliobackend/security/controller/AuthController.java
+++ src/main/java/com/raama/portfoliobackend/security/controller/AuthController.java
@@ -30,7 +30,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin("*")
+@CrossOrigin(origins = "*")
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
