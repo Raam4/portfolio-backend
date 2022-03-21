f37b93aa16ec6cbe75a0670102356f40d1515985
Raam4 <ramiro_cardozo22@hotmail.com>
2022-03-21 15:59:14 -0300

    fix

--- src/main/java/com/raama/portfoliobackend/security/MainSecurity.java
+++ src/main/java/com/raama/portfoliobackend/security/MainSecurity.java
@@ -40,7 +40,7 @@ public class MainSecurity extends WebSecurityConfigurerAdapter{
         http.cors().and().csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/auth/**").permitAll()
-                .antMatchers("*").permitAll()
+                .antMatchers().permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .exceptionHandling().authenticationEntryPoint(jwtEntryPoint)
